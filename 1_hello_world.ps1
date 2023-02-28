b3857614283587fd4c0ef5362384237f75c04e1e
njain51 <official.nitinjain@gmail.com>
2023-02-28 18:59:35 -0500

    Added new PowerShell sample scripts

---
+++ 1_hello_world.ps1
@@ -0,0 +1,6 @@
+﻿cd \
+cd D:\mylearning\SAMPLE_PROJECTS\pshell_sample
+mkdir sample_programs
+cd sample_programs
+New-Item hello_world.txt
+Set-Content hello_world.txt "this is my first PowerShell Program"
