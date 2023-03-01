6bcefb0885da103354efbb916f3c13a9fa83011b
njain51 <official.nitinjain@gmail.com>
2023-02-28 19:47:23 -0500

    Added new PowerShell sample scripts

---
+++ 1_working_programs/5_ifl_else_1.ps1
@@ -0,0 +1,31 @@
+﻿# this sample program get age input from user and output appropriate classification accordingly
+
+$a = Read-Host("Enter your age")
+$a
+
+if($a -le 14){
+        Write-Host("Your age is $a and you belong to Pediatric Group")
+    }
+
+elseif (($a -le 47) -and ($a -ge 15)){
+    
+
+        Write-Host("Your age is $a and you belong to young Group")
+
+    }
+
+elseif(($a -le 63) -and ($a -ge 48)){
+
+        Write-Host("Your age is $a and you belong to middle age Group")
+
+    }
+elseif($a -ge 64){
+
+        Write-Host("Your age is $a and you belong to elderly Group")
+
+    }
+else{
+
+        Write-Host("Your entered $a and its not a valid age")
+
+    }
