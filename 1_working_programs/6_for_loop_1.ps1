ea4f981d3757d803b3df334e7d0d256e4618c9d7
njain51 <official.nitinjain@gmail.com>
2023-02-28 20:13:54 -0500

    Added new PowerShell sample scripts

---
+++ 1_working_programs/6_for_loop_1.ps1
@@ -0,0 +1,11 @@
+﻿# Print "*" half diagonal based on number of lines input by user:
+
+$num_lines=Read-Host("Enter number of lines")
+
+for($i=0;$i -le $num_lines; $i++){
+    Write-Host(" " * ($num_lines-$i))
+    Write-Host("*" * (2*$i+1))
+    
+}
+
+ 
