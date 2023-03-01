2a96cb9751397907e7a99529b0e2aecf3f1186b0
njain51 <official.nitinjain@gmail.com>
2023-02-28 20:43:00 -0500

    Added new PowerShell sample scripts

--- 1_working_programs/7_function_1.ps1
+++ 1_working_programs/7_function_1.ps1
@@ -1,8 +1,8 @@
 ﻿# sample program to demonstrate function in powershell
 
 function add-num{
-    $a=2
-    $b=5
+    [int]$a=Read-Host -Prompt "Enter first number"
+    [int]$b=Read-Host -Prompt "Enter second number"
     $res=$a+$b
     Write-Host("Result is $res")
 }
---
+++ 1_working_programs/8_arrays_1.ps1
@@ -0,0 +1,18 @@
+﻿# declare arrays and print value at index 
+
+$a=@('1','2','3','4','5','65','6','8','61','4')
+ 
+Write-Host("Value at index 3 is ") $a[3]
+
+# declare array and print index
+
+$laptops=@("one","two","three")
+$laptops
+# adding new laptop
+$laptops=$laptops + "four"
+
+Write-Host("Value at index 1 is ") $laptops[3]
+
+# delete laptop
+$laptops = $laptops | where {$_ -ne "three"}
+$laptops
