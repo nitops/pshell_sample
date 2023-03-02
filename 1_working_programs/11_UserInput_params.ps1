3d09386d6c791d302e546bf5efb7f3cfac119924
njain51 <official.nitinjain@gmail.com>
2023-03-02 18:33:06 -0500

    Added new PowerShell sample scripts

--- 1_working_programs/11_UserInput_params.ps1
+++ 1_working_programs/11_UserInput_params.ps1
@@ -1,25 +1,28 @@
 ﻿# Program to demonstrate user input in powershell
-cls
-$infoSelection = Read-Host -Prompt "For C Drive Space enter '1'; For RAM usage information enter '2'"
 
-if($infoSelection -eq 1){
-    Write-Host "You Selected '1' to know about C Drive Space"
+param(
+    [switch] $freespace,
+    [switch] $memoryusage
+)
+
+if($freespace){
+ 
     $drive = Get-Volume | Where-Object DriveLetter -EQ 'C'
-    $freespace = [math]::Round(($drive.SizeRemaining/1GB),2) 
-    Write-Host "Size remaining for C drive is $freespace GB"
+    $calc_freespace = [math]::Round(($drive.SizeRemaining/1GB),2) 
+    Write-Host "Size remaining for C drive is $calc_freespace GB"
 
 
 }
-elseif($infoSelection -eq 2){
-    Write-Host "You Selected '2' to know about RAM usage information"
-    $totalRAM = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | Foreach {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))}
+if($memoryusage){
+ 
+    $totalRAM = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))}
     Write-Host "Total RAM size is $totalRAM GB"
 
     $CompObject = Get-WmiObject -Class Win32_OperatingSystem
-    $RAMUsed = [System.Math]::Round(($CompObject.FreePhysicalMemory/1024/1024),2)
-    Write-Host "RAM used is $RAMUsed GB out of total $totalRAM GB"
-
+    $calc_RAMUsed = [System.Math]::Round(($CompObject.FreePhysicalMemory/1024/1024),2)
+    Write-Host "RAM used is $calc_RAMUsed GB out of total $totalRAM GB"
 }
-else{
-    Write-Host "You have selected wrong input"
+elseif ($freespace -ne $true -and $memoryusage -ne $true) {
+    Write-Host "please select a parameter"
 }
+ 
---
+++ 1_working_programs/12_CmdletBinding_demo.ps1
@@ -0,0 +1,36 @@
+﻿#example:
+[CmdletBinding(PositionalBinding=$true)]
+
+# Parameter help description
+param(
+    [Parameter(Mandatory=$true)]
+    [Alias('hostname')]
+    [ValidateLength(9,12)]
+    [string]$computerName,
+    
+    [Parameter(Mandatory=$false)]
+    [string]$outputfilePath
+)
+
+
+Write-Verbose "Querying wmi from $computerName"
+$os = Get-WmiObject -Class WIN32_OperatingSystem -ComputerName $computerName -EA stop -EV x
+$totalRam = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))}
+# PS custom object
+$obj = [PSCustomObject]@{
+    ComputerName = $computerName;
+    OS_Version = $os.caption
+    Manufacturer = $computerName.Manufacturer;
+    totalRam = $totalRam
+}
+
+#Output to csv file if path is supplied
+
+if($outputfilePath -ne ""){
+    $obj | Export-Csv -Path $outputfilePath  -NoTypeInformation -Append
+    Write-Output $obj
+}
+else{
+    Write-Output $obj
+}
+
